cca49069b70f05cbc935dc5a849cd1989f008492
Et-Shafa <naadiramalaika.atsiila@gmail.com>
2022-12-02 15:53:55 +0700

    add profile menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
-import { Header , MainContainer , CreateContainer} from './components'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { Header, MainContainer, CreateContainer } from "./components";
 
 const App = () => {
   return (
-    <AnimatePresence>
-    <div className="flex flex-col w-screen h-auto bg-primary">
-      <Header />
-      <main className='w-full p-8 mt-24'>
-        <Routes>
-          <Route path="/*" element={<MainContainer />} />
-          <Route path="/createItem" element={<CreateContainer />} />
-        </Routes>
-      </main>
-    </div>
+    <AnimatePresence mode="wait">
+      <div className="flex flex-col w-screen h-auto bg-primary">
+        <Header />
+        <main className="w-full p-8 mt-24">
+          <Routes>
+            <Route path="/*" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
+          </Routes>
+        </main>
+      </div>
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,45 @@
 /* eslint-disable */
-import React from 'react'
+import React, { useState } from "react";
 
-import Logo from '../img/logo.png'
-import Avatar from '../img/avatar.png'
-import {MdShoppingCart} from 'react-icons/md'
+import Logo from "../img/logo.png";
+import Avatar from "../img/avatar.png";
+import { MdShoppingCart, MdAdd, MdLogout } from "react-icons/md";
 
-import { motion } from 'framer-motion'
-import { Link } from 'react-router-dom'
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { app } from '../firebase.config';
-import { useStateValue } from '../context/StateProvider';
-import { actionType } from '../context/reducer';
+import { app } from "../firebase.config";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
 
 const header = () => {
-
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
-  const [{user}, dispatch] = useStateValue()
+  const [{ user }, dispatch] = useStateValue();
+
+  const [isMenu, setisMenu] = useState(false);
 
   const login = async () => {
-    const {user:{refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider);
-    dispatch({
-      type:actionType.SET_USER,
-      user: providerData[0]
-    });
-    localStorage.setItem("user", JSON.stringify(providerData[0]));
+    if (!user) {
+      const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    } else {
+      setisMenu(!isMenu);
+    }
   };
 
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
-
       {/* desktop and tablet */}
       <div className="items-center justify-between hidden w-full h-full md:flex">
         <Link to={"/"} className="flex items-center gap-2">
@@ -39,23 +47,56 @@ const header = () => {
           <p className="text-xl font-bold text-headingColor">City</p>
         </Link>
 
-        <div className='flex items-center gap-8'>
-        <ul className="flex items-center gap-8">
-          <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor">Home</li>
-          <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor">Menu</li>
-          <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor">About Us</li>
-          <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor">Service</li>
-        </ul>
-        <div className="relative flex items-center justify-center">
-          <MdShoppingCart className="text-2xl cursor-pointer text-textColor"/>
-          <div className='absolute flex items-center justify-center w-5 h-5 rounded-full -top-2 -right-2 bg-cartNumBg'>
-            <p className='text-xs font-semibold text-white'>2</p>
+        <div className="flex items-center gap-8">
+          <ul className="flex items-center gap-8">
+            <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor">
+              Home
+            </li>
+            <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor">
+              Menu
+            </li>
+            <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor">
+              About Us
+            </li>
+            <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor">
+              Service
+            </li>
+          </ul>
+          <div className="relative flex items-center justify-center">
+            <MdShoppingCart className="text-2xl cursor-pointer text-textColor" />
+            <div className="absolute flex items-center justify-center w-5 h-5 rounded-full -top-2 -right-2 bg-cartNumBg">
+              <p className="text-xs font-semibold text-white">2</p>
+            </div>
           </div>
-        </div>
 
-        <div className="relative">
-        <motion.img whileTap={{ scale: 0.6 }} src={user ? user.photoURL : Avatar} className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full" alt="userprofile" onClick={login} />
-        </div>
+          <div className="relative">
+            <motion.img
+              whileTap={{ scale: 0.6 }}
+              src={user ? user.photoURL : Avatar}
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+              alt="userprofile"
+              onClick={login}
+            />
+            {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-x1 rounded-lg flex flex-col absolute top-12 right-0"
+              >
+                {user && user.email === "naadiramalaika.atsiila@gmail.com" && (
+                  <Link to={"/createItem"}>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textcolor text-base">
+                      <MdAdd /> New Item
+                    </p>
+                  </Link>
+                )}
+                <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textcolor text-base">
+                  <MdLogout /> Log Out
+                </p>
+              </motion.div>
+            )}
+          </div>
         </div>
       </div>
 
@@ -65,4 +106,4 @@ const header = () => {
   );
 };
 
-export default header
+export default header;
