a0769c377afeed835cc6f6d291d8fb639d0b25db
sensasicool <fariz.rahman-2020@vokasi.unair.ac.id>
2022-12-08 01:38:36 +0700

    edit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,12 +48,12 @@ const header = () => {
   };
 
   return (
-    <header className="fixed z-50 w-screen p-4 px-5 md:p-6 md:px-16 bg-primary ">
+    <header className="fixed z-50 w-screen p-4 px-5 md:p-6 md:px-16 bg-black ">
       {/* desktop and tablet */}
       <div className="items-center justify-between hidden w-full h-full md:flex">
         <Link to={"/"} className="flex items-center gap-2">
           <img src={navbar_icon} className="object-cover w-8" alt="logo" />
-          <p className="text-xl font-bold text-headingColor">Bar Beninx</p>
+          <p className="text-xl font-bold text-white">Bar Beninx</p>
         </Link>
 
         <div className="flex items-center gap-8">
@@ -63,23 +63,35 @@ const header = () => {
             exit={{ opacity: 0, x: 200 }}
             className="flex items-center gap-8"
           >
-            <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor" onClick={()=>setisMenu(false)}>
+            <li
+              className="text-base transition-all duration-100 ease-in-out cursor-pointer text-white hover:text-headingColor"
+              onClick={() => setisMenu(false)}
+            >
               Home
             </li>
-            <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor" onClick={()=>setisMenu(false)}>
+            <li
+              className="text-base transition-all duration-100 ease-in-out cursor-pointer text-white hover:text-headingColor"
+              onClick={() => setisMenu(false)}
+            >
               Menu
             </li>
-            <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor" onClick={()=>setisMenu(false)}>
+            <li
+              className="text-base transition-all duration-100 ease-in-out cursor-pointer text-white hover:text-headingColor"
+              onClick={() => setisMenu(false)}
+            >
               About Us
             </li>
-            <li className="text-base transition-all duration-100 ease-in-out cursor-pointer text-textColor hover:text-headingColor" onClick={()=>setisMenu(false)}>
+            <li
+              className="text-base transition-all duration-100 ease-in-out cursor-pointer text-white hover:text-headingColor"
+              onClick={() => setisMenu(false)}
+            >
               Service
             </li>
           </motion.ul>
           <div className="relative flex items-center justify-center">
-            <MdShoppingCart className="text-2xl cursor-pointer text-textColor" />
-            <div className="absolute flex items-center justify-center w-5 h-5 rounded-full -top-2 -right-2 bg-cartNumBg">
-              <p className="text-xs font-semibold text-white">2</p>
+            <MdShoppingCart className="text-2xl cursor-pointer text-white" />
+            <div className="absolute flex items-center border border-black justify-center w-5 h-5 rounded-full -top-2 -right-2 bg-white">
+              <p className="text-xs font-semibold text-black">2</p>
             </div>
           </div>
 
@@ -100,7 +112,10 @@ const header = () => {
               >
                 {user && user.email === "barbeninx@gmail.com" && (
                   <Link to={"/createItem"}>
-                    <p className="flex items-center gap-3 px-4 py-2 text-base transition-all duration-100 ease-in-out rounded-t-lg cursor-pointer hover:bg-slate-100 text-textcolor" onClick={()=>setisMenu(false)}>
+                    <p
+                      className="flex items-center gap-3 px-4 py-2 text-base transition-all duration-100 ease-in-out rounded-t-lg cursor-pointer hover:bg-slate-100 text-textcolor"
+                      onClick={() => setisMenu(false)}
+                    >
                       <MdAdd /> New Item
                     </p>
                   </Link>
@@ -148,7 +163,10 @@ const header = () => {
             >
               {user && user.email === "barbeninx@gmail.com" && (
                 <Link to={"/createItem"}>
-                  <p className="flex items-center gap-3 px-4 py-2 text-base transition-all duration-100 ease-in-out rounded-t-lg cursor-pointer hover:bg-slate-100 text-textColor" onClick={()=>setisMenu(false)}>
+                  <p
+                    className="flex items-center gap-3 px-4 py-2 text-base transition-all duration-100 ease-in-out rounded-t-lg cursor-pointer hover:bg-slate-100 text-textColor"
+                    onClick={() => setisMenu(false)}
+                  >
                     New Item <MdAdd />
                   </p>
                 </Link>
--- src/utils/data.js
+++ src/utils/data.js
@@ -3,32 +3,51 @@ import F1 from "../img/bar_beninx/vodka/smarty.png";
 import C3 from "../img/bar_beninx/wine/syrah.png";
 import Fi1 from "../img/bar_beninx/wine/blanc.png";
 
-
 export const heroData = [
-
-  { id : 1, name : 'Jackues', decp : 'Absinthe famous in western', price : '323.000', imageSrc : I1 ,},
-  { id : 2, name : 'Smarty', decp : 'Vodka inject from uni soviet', price : '120.000', imageSrc : F1 ,},
-  { id : 3, name : 'Syrah', decp : 'The best wine in arabian tale', price : '215.000', imageSrc : C3 ,},
-  { id : 4, name : 'Blanc', decp : 'from the land of beer "german"', price : '280.000', imageSrc : Fi1 ,},
-
+  {
+    id: 1,
+    name: "Jackues",
+    decp: "Absinthe famous in western",
+    price: "323.000",
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Smarty",
+    decp: "Vodka inject from uni soviet",
+    price: "120.000",
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: "Syrah",
+    decp: "The best wine in arabian tale",
+    price: "215.000",
+    imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: "Blanc",
+    decp: 'from the land of beer "german"',
+    price: "280.000",
+    imageSrc: Fi1,
+  },
 ];
 
 export const categories = [
-
-  { id : 1, name : 'Wine', urlParamName: "wine",},
-  { id : 2, name : 'Bir', urlParamName: "bir",},
-  { id : 3, name : 'Sake', urlParamName: "sake",},
-  { id : 4, name : 'Gin', urlParamName: "gin",},
-  { id : 5, name : 'Tequilla', urlParamName: "tequilla",},
-  { id : 6, name : 'Brandy', urlParamName: "brandy",},
-  { id : 7, name : 'Whiskey', urlParamName: "whiskey",},
-  { id : 8, name : 'Vodka', urlParamName: "vodka",},
-  { id : 9, name : 'Rum', urlParamName: "rum",},
-  { id : 10, name : 'Soju', urlParamName: "soju",},
-  { id : 11, name : 'Absinthe', urlParamName: "absinthe",},
-  { id : 12, name : 'Mocktail', urlParamName: "mocktail",},
-  { id : 13, name : 'Soft drink', urlParamName: "soft drink",},
-  { id : 14, name : 'Coffe & tea', urlParamName: "coffe & tea",},
-  { id : 15, name : 'Juice', urlParamName: "juice",},
-
+  { id: 1, name: "Wine", urlParamName: "wine" },
+  { id: 2, name: "Bir", urlParamName: "bir" },
+  { id: 3, name: "Sake", urlParamName: "sake" },
+  { id: 4, name: "Gin", urlParamName: "gin" },
+  { id: 5, name: "Tequilla", urlParamName: "tequilla" },
+  { id: 6, name: "Brandy", urlParamName: "brandy" },
+  { id: 7, name: "Whiskey", urlParamName: "whiskey" },
+  { id: 8, name: "Vodka", urlParamName: "vodka" },
+  { id: 9, name: "Rum", urlParamName: "rum" },
+  { id: 10, name: "Soju", urlParamName: "soju" },
+  { id: 11, name: "Absinthe", urlParamName: "absinthe" },
+  { id: 12, name: "Mocktail", urlParamName: "mocktail" },
+  { id: 13, name: "Soft drink", urlParamName: "soft drink" },
+  { id: 14, name: "Coffe & tea", urlParamName: "coffe & tea" },
+  { id: 15, name: "Juice", urlParamName: "juice" },
 ];
